5ad3b72d937bf0d25cbfd7fd4db51cfbea165c81
Ali Panah <ayp@fb.com>
2015-05-27 15:20:20 -0700

    first commit

--- ProductsOfPrimes/main.m
+++ ProductsOfPrimes/main.m
@@ -1,17 +1,27 @@
-//
-//  main.m
-//  ProductsOfPrimes
-//
-//  Created by Ali Panah on 5/27/15.
-//  Copyright (c) 2015 Ali Panah. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+void printProducts(NSArray *numberArray, NSUInteger idx, NSUInteger prod){
+  
+  if (idx==numberArray.count) {
+    if(prod != 1){
+      NSLog(@"%lu",(unsigned long)prod);
+    }
+    return;
   }
-    return 0;
+  
+  printProducts(numberArray, idx+1, prod);
+  printProducts(numberArray, idx+1, [numberArray[idx] integerValue]*prod);
+  
+  return;
+}
+
+int main(int argc, const char *argv[]){
+  @autoreleasepool {
+    
+    NSArray *numberArray = @[@2,@3,@5];
+    printProducts(numberArray,0,1);
+    
+  }
+  
+  return 0;
 }
