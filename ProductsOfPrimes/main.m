0e614bca91661a9de210f3eb0d39ed835adeef1e
Ali Panah <ayp@fb.com>
2015-05-27 17:32:29 -0700

    Added iterative method

--- ProductsOfPrimes/main.m
+++ ProductsOfPrimes/main.m
@@ -1,25 +1,48 @@
 #import <Foundation/Foundation.h>
 
-void printProducts(NSArray *numberArray, NSUInteger idx, NSUInteger prod){
+void printProductsRecursive(NSArray *numberArray, NSUInteger idx, NSUInteger prod){
   
   if (idx==numberArray.count) {
     if(prod != 1){
-      NSLog(@"%lu",(unsigned long)prod);
+      NSLog(@"Recursive:%lu",(unsigned long)prod);
     }
     return;
   }
   
-  printProducts(numberArray, idx+1, prod);
-  printProducts(numberArray, idx+1, [numberArray[idx] integerValue]*prod);
+  printProductsRecursive(numberArray, idx+1, prod);
+  printProductsRecursive(numberArray, idx+1, [numberArray[idx] integerValue]*prod);
   
   return;
 }
 
-int main(int argc, const char *argv[]){
+void printProductsIterative(NSArray *numberArray){
+  
+  for (int i=1; i<=(1<<numberArray.count)-1; i++) {
+    
+    NSMutableSet *subset = [[NSMutableSet alloc] init];
+    
+    for (int j=0; j<=numberArray.count-1; j++) {
+      if((1<<j)& i){
+        [subset addObject:[numberArray objectAtIndex:j]];
+      }
+    }
+    
+    double prod = 1;
+    for (NSNumber *num in subset) {
+      prod = prod*[num integerValue];
+    }
+    NSLog(@"Iterative:%.0f",prod);
+  }
+  
+  return;
+}
+
+int main(int argc, const char *argv[]) {
   @autoreleasepool {
     
-    NSArray *numberArray = @[@2,@3,@5];
-    printProducts(numberArray,0,1);
+    NSArray *numberArray = @[@2,@11,@3,@5,@7,@13];
+    printProductsRecursive(numberArray,0,1);
+    printProductsIterative(numberArray);
     
   }
   
